a440fdaa5f82e32b78d9d79fad2720c93fba65a1
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-10-31 14:57:31 -0400

    Yeah...

--- include/jasmine/search.hpp
+++ include/jasmine/search.hpp
@@ -2,7 +2,7 @@
 #include <Geode/binding/GJSearchObject.hpp>
 
 namespace jasmine::search {
-    template <std::input_iterator It, std::sentinel_for<It> Sent, class Func>
+    template <std::input_iterator It, std::sentinel_for<It> Sent, class Func = std::identity>
     requires std::is_invocable_r_v<std::string, Func, typename std::iterator_traits<It>::value_type>
     GJSearchObject* getObject(It begin, Sent end, Func func = {}) {
         auto obj = GJSearchObject::create(SearchType::Type19);
@@ -15,7 +15,7 @@ namespace jasmine::search {
         return obj;
     }
 
-    template <std::ranges::input_range Rng, class Func>
+    template <std::ranges::input_range Rng, class Func = std::identity>
     requires std::is_invocable_r_v<std::string, Func, typename std::ranges::range_value_t<Rng>>
     GJSearchObject* getObject(Rng&& range, Func func = {}) {
         return getObject(std::ranges::begin(range), std::ranges::end(range), std::move(func));
