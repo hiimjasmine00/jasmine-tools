e22fd82863f08a36dc41993c129eeeb3e909d904
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-11-01 04:44:46 -0400

    I HATE YOU

--- src/jasmine.cpp
+++ src/jasmine.cpp
@@ -138,7 +138,7 @@ bool TableNode::init(int columns, int rows, float width, float height, std::stri
     m_columns = columns;
 
     for (int i = 0; i < rows; i++) {
-        auto menu = CCNode::create();
+        auto menu = CCMenu::create();
         menu->setContentSize({ width, height / rows });
         menu->setLayout(RowLayout::create()->setAxisAlignment(AxisAlignment::Even));
         menu->setID(fmt::format("{}-{}", prefix, i + 1));
