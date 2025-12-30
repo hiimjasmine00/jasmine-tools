1019a5bb7d985f5e6c59fa1e385a8d8af51923b7
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-12-30 09:57:32 -0500

    The Fast and the Float

--- include/jasmine/convert.hpp
+++ include/jasmine/convert.hpp
@@ -1,38 +1,21 @@
 #pragma once
-#ifdef __cpp_lib_to_chars
-#include <charconv>
-#else
-#include <Geode/utils/general.hpp>
-#endif
+#include <fast_float/fast_float.h>
 
 namespace jasmine::convert {
-    template <std::integral T>
-    bool toInt(std::string_view str, T& out) {
-        return std::from_chars(str.data(), str.data() + str.size(), out).ec == std::errc();
+    template <class T>
+    bool to(std::string_view str, T& out) {
+        return fast_float::from_chars(str.data(), str.data() + str.size(), out).ec == std::errc();
     }
 
-    template <std::integral T>
-    std::optional<T> getInt(std::string_view str) {
+    template <class T>
+    std::optional<T> get(std::string_view str) {
         T value = {};
-        return toInt(str, value) ? std::optional<T>(value) : std::nullopt;
+        return to(str, value) ? std::optional<T>(value) : std::nullopt;
     }
 
-    template <std::floating_point T>
-    bool toFloat(std::string_view str, T& out) {
-        #ifdef __cpp_lib_to_chars
-        return std::from_chars(str.data(), str.data() + str.size(), out).ec == std::errc();
-        #else
-        if (geode::Result<T> res = geode::utils::numFromString<T>(str)) {
-            out = res.unwrap();
-            return true;
-        }
-        return false;
-        #endif
-    }
-
-    template <std::floating_point T>
-    std::optional<T> getFloat(std::string_view str) {
-        T value = {};
-        return toFloat(str, value) ? std::optional<T>(value) : std::nullopt;
+    template <class T>
+    T getOr(std::string_view str, T defaultValue = {}) {
+        T value;
+        return to(str, value) ? value : defaultValue;
     }
 }
